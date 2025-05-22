7d536a8a170a9f1cbeafc2e5560a13286c0ebb4f
Your Name <you@example.com>
2025-05-22 13:13:41 -0300

    cont correciones

--- controllers/productCont.js
+++ controllers/productCont.js
@@ -1,6 +1,18 @@
+const data = require('../db/modulo_datos');
+
 const productCont = {
     index: function(req, res) {
-        res.render('product');
+        res.render('index', { productos: data.productos });
+    },
+    productDetail: function(req, res) {
+        const id = parseInt(req.params.id);
+        const product = data.productos.find(p => p.id === id);
+
+        if (!product) {
+            return res.status(404).send('Producto no encontrado');
+        }
+
+        res.render('product', { product });
     },
     productAdd: function(req, res) {
         res.render('product-add');
--- controllers/userCont.js
+++ controllers/userCont.js
@@ -9,7 +9,27 @@ const userCont = {
     },
     profile: function(req, res) {
         res.render('profile', { usuario: data.usuario });
+    },
+    
+    createLogin: function (req,res) {
+        let userInfo = {
+            email: req.body.email,
+            password: req.body.password,
+            recordarme: req.body.recordarme
+        }
+        req.session.user = userInfo;
+    
+    if (userInfo.recordarme != undefined) {
+        res.cookie("user", userInfo, {maxAge: 1000 * 60 * 5})
+        
     }
+        
+    }
+    
+    
 }
+
+
+
 
 module.exports = userCont;
